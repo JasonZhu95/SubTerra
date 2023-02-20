9037f8b85896de5458523e3f62b47655dc58819e
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2023-02-20 08:50:57 -0800

    Reorganized Folder Organization, Set Up Death Core Component

---
+++ Assets/_Scripts/Core/Core.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Core : MonoBehaviour
+{
+    private readonly List<CoreComponent> CoreComponents = new List<CoreComponent>();
+
+    private void Awake()
+    {
+
+    }
+
+    public void LogicUpdate()
+    {
+        foreach (CoreComponent component in CoreComponents)
+        {
+            component.LogicUpdate();
+        }
+    }
+
+    public void AddComponent(CoreComponent component)
+    {
+        if (!CoreComponents.Contains(component))
+        {
+            CoreComponents.Add(component);
+        }
+    }
+
+    // FUNCTION: Accesses the List of Core Components and returns the type of component.
+    public T GetCoreComponent<T>() where T : CoreComponent
+    {
+        var comp = CoreComponents.OfType<T>().FirstOrDefault();
+        if (comp)
+            return comp;
+
+        comp = GetComponentInChildren<T>();
+
+        if (comp)
+            return comp;
+
+        Debug.LogWarning($"{typeof(T)} not found on {transform.parent.name}");
+
+        return null;
+    }
+
+    public T GetCoreComponent<T>(ref T value) where T : CoreComponent
+    {
+        value = GetCoreComponent<T>();
+        return value;
+    }
+}
---
+++ Assets/_Scripts/Player/PlayerStates/SubStates/PlayerAttackState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackState : PlayerAbilityState
+{
+    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+}
