450bd55cc6f4de278d66f1b8a10edcee7d086d07
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2023-03-02 18:34:25 -0800

    Added Selection of Inventory items displaying text and description ui

--- Assets/_Scripts/UI/InventoryUI/InventoryController.cs
+++ Assets/_Scripts/UI/InventoryUI/InventoryController.cs
@@ -3,20 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using Project.EventChannels;
 using Project.Managers;
+using System;
 
 public class InventoryController : MonoBehaviour
 {
-    [SerializeField] private InventoryPage inventoryUI;
+    [SerializeField] private UIInventoryPage inventoryUI;
+
+    [SerializeField] private InventorySO inventoryData;
 
     [SerializeField] private GameStateEventChannel GameStateEventChannel;
 
     public PlayerInputHandler InputHandler { get; private set; }
 
-    public int inventorySize = 10;
-
     private void Start()
     {
-        inventoryUI.InitializeInventoryUI(inventorySize);
+        PrepareUI();
+        //inventoryData.Initialize();
+    }
+
+    private void PrepareUI()
+    {
+        inventoryUI.InitializeInventoryUI(inventoryData.Size);
+        this.inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
+        this.inventoryUI.OnSwapItems += HandleSwapItems;
+        this.inventoryUI.OnStartDragging += HandleDragging;
+        this.inventoryUI.OnItemActionRequested += HandleItemActionRequest;
+    }
+
+    private void HandleItemActionRequest(int itemIndex)
+    {
+    }
+
+    private void HandleDragging(int itemIndex)
+    {
+    }
+
+    private void HandleSwapItems(int itemIndexOne, int itemIndexTwo)
+    {
+
+    }
+
+    private void HandleDescriptionRequest(int itemIndex)
+    {
+        InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+        if (inventoryItem.IsEmpty)
+        {
+            inventoryUI.ResetSelection();
+            return;
+        } 
+        ItemSO item = inventoryItem.item;
+        inventoryUI.UpdateDescription(itemIndex, item.ItemImage,
+            item.name, item.Description);
     }
 
     private void Awake()
@@ -28,8 +65,17 @@ public class InventoryController : MonoBehaviour
     {
         if (InputHandler.EscapePressed)
         {
-            inventoryUI.Show();
-            GameStateEventChannel.RaiseSetChangeGameStateEvent(this, new GameStateEventArgs(GameState.UI));
+            if (inventoryUI.isActiveAndEnabled == false)
+            {
+                inventoryUI.Show();
+                foreach (var item in inventoryData.GetCurrentInventoryState())
+                {
+                    inventoryUI.UpdateData(item.Key,
+                        item.Value.item.ItemImage,
+                        item.Value.quantity);
+                }
+                GameStateEventChannel.RaiseSetChangeGameStateEvent(this, new GameStateEventArgs(GameState.UI));
+            }
         }
         else
         {
---
+++ Assets/_Scripts/UI/InventoryUI/UIInventoryItem.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.EventSystems;
+
+public class UIInventoryItem : MonoBehaviour, IPointerClickHandler,
+    IBeginDragHandler, IEndDragHandler, IDropHandler, IDragHandler
+{
+    [SerializeField]
+    private Image itemImage;
+    [SerializeField]
+    private TMP_Text quantityTxt;
+
+    [SerializeField]
+    private Image borderImage;
+
+    public event Action<UIInventoryItem> OnItemClicked,
+        OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
+        OnRightMouseBtnClick;
+
+    private bool empty = true;
+
+    public void Awake()
+    {
+        ResetData();
+        Deselect();
+    }
+    public void ResetData()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        this.empty = true;
+    }
+    public void Deselect()
+    {
+        this.borderImage.enabled = false;
+    }
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityTxt.text = quantity + "";
+        this.empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    public void OnPointerClick(PointerEventData pointerData)
+    {
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+}
