6d9f21a87f85c48bfc3cecd753d6fa674963b1aa
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2023-03-28 19:20:04 -0700

    Added a confirmation for returning to menu from pause

--- Assets/_Scripts/UI/PauseUI/PauseUI.cs
+++ Assets/_Scripts/UI/PauseUI/PauseUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using Project.LevelSetup;
+using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
@@ -11,22 +12,34 @@ public class PauseUI : MonoBehaviour
     private GameObject pauseMenu;
 
     [SerializeField]
+    private GameObject confirmationMenu;
+
+    [SerializeField]
     private GameObject firstSelectedButton;
 
+    [SerializeField]
+    private GameObject firstSelectedConfirmationButton;
+
     private Animator anim;
+    private Animator animConfirmation;
     private LevelLoaderManager levelLoaderManager;
 
     private bool isPaused;
 
     public PlayerInputHandler InputHandler { get; private set; }
 
+    private Color newColor;
+    private Image pauseMenuImage;
 
     private void Awake()
     {
         pauseMenu.SetActive(false);
         anim = pauseMenu.GetComponent<Animator>();
+        animConfirmation = confirmationMenu.GetComponent<Animator>();
         InputHandler = GameObject.FindWithTag("Player").GetComponent<PlayerInputHandler>();
         levelLoaderManager = GameObject.Find("LevelLoader").GetComponent<LevelLoaderManager>();
+        pauseMenuImage = pauseMenu.GetComponent<Image>();
+        newColor = pauseMenuImage.color;
     }
 
     private void Update()
@@ -57,10 +70,28 @@ public class PauseUI : MonoBehaviour
     public void ResumeGame()
     {
         anim.SetBool("pause", false);
+        anim.SetBool("pauseFromConfirm", false);
         InputHandler.PausePressed = false;
         InputHandler.BlockActionInput = false;
         InputHandler.SwitchToActionMap("Gameplay");
         isPaused = false;
+    }
+
+    public void StartConfirmationMenu()
+    {
+        pauseMenu.SetActive(false);
+        confirmationMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(firstSelectedConfirmationButton);
+        animConfirmation.SetBool("start", true);
+    }
+
+    public void CancelConfirmationMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+        pauseMenu.SetActive(true);
+        anim.SetBool("pauseFromConfirm", true);
+        animConfirmation.SetBool("start", false);
+        confirmationMenu.SetActive(false);
     }
 
     public void ReturnToMenu()
