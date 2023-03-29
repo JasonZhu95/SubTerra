59c5f69cad7a15c82d2866144edaedb5d4be0ee3
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2023-03-28 17:36:04 -0700

    Added select on mouse hover

--- Assets/_Scripts/UI/ButtonArrowHandler.cs
+++ Assets/_Scripts/UI/ButtonArrowHandler.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ButtonArrowHandler : MonoBehaviour
+public class ButtonArrowHandler : MonoBehaviour, IPointerEnterHandler
 {
     private GameObject arrowContainer;
+    private EventSystem eventSystem;
 
     private void Awake()
     {
         arrowContainer = gameObject.transform.GetChild(0).gameObject;
+    }
+
+    private void OnEnable()
+    {
+        eventSystem = EventSystem.current;
     }
 
     private void Update()
@@ -22,5 +28,10 @@ public class ButtonArrowHandler : MonoBehaviour
         {
             arrowContainer.SetActive(false);
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        eventSystem.SetSelectedGameObject(gameObject);
     }
 }
